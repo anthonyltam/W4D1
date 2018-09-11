a25cf3559074c4a217957d2088f24f0e82a30acc
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-09-10 18:08:25 -0700

    do stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,21 @@ class UsersController < ApplicationController
   end
   
   def create
-    render json: params
+    user = User.new(params.require(:user).permit(:name, :email))
+    # replace the `user_attributes_here` with the actual attribute keys
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
   
   def show
-    render json: params
+    user = User.find(params[:id])
+    if User.find(params[:id])
+      render json: user
+    else
+      render json: user.errors.full_messages, status: 404
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_10_223017) do
+ActiveRecord::Schema.define(version: 2018_09_10_233904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.string "email", null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
 end
